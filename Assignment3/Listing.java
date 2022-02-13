974c8c04fdbc7172ea5c16441e4dc3faf7b7ca2c
LeahKupfer <s01221525@acad.tri-c.edu>
2022-02-13 00:05:37 -0500

    main

---
+++ Assignment3/Listing.java
@@ -0,0 +1,32 @@
+/* 
+*Leah Kupfer
+ */
+
+public class Listing {
+    private String name;
+    private String address;
+    private String number;
+
+    public String toString() {
+        return ("Name is " + name + "\n" +
+                "Address is " + address + "\n" +
+                "Number is " + number + "\n");
+    }
+
+    public Listing deepCopy() {
+        Listing clone = new Listing(name, address, number);
+        return clone;
+    }
+
+    public Listing() {
+        name = "";
+        address = "";
+        number = "";
+    }
+
+    public Listing(String n, String a, String num) {
+        name = n;
+        address = a;
+        number = num;
+    }
+}
---
+++ Assignment3/Main.java
@@ -0,0 +1,25 @@
+/*  
+* Leah Kupfer
+ */
+
+//Driver class to test Problems 19 & 20
+public class Main {
+
+    public static void main(String[] args) {
+        Stack s = new Stack();
+        System.out.print("The Stack is empty: " + s.isEmpty() + "\n");
+        s.push(new Listing("bob", "1 Main St", "256"));
+        s.push(new Listing("jim", "2 Main St", "648"));
+        System.out.print("The Stack is empty: " + s.isEmpty() + "\n");
+        s.push(new Listing("abe", "3 Main St", "986"));
+        s.showAll();
+        System.out.print("The Stack is full: " + s.isFull() + "\n");
+
+        s.push(new Listing("emily", "4 Main St", "658"));
+        s.showAll();
+        System.out.print("\n" + s.peek() + "\n");
+        s.showAll();
+        s.reinit();
+        System.out.print("The Stack is empty: " + s.isEmpty());
+    }
+}
